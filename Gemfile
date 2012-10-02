bec4ffe9753f6ba98bcc73aa13ba1406f86ab7fb
TrailHacker <lucas@hardbarger.com>
2012-10-01 22:20:27 -0400

    Added mechanic to scrap site and included app config for url to scrape

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'mechanize', '2.5.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,9 @@
 class RacesController < ApplicationController
+
+  def index
+    url = APP_CONFIG['ultra_marathon_results_uri']
+    respond_to do |format|
+      format.json { render json: { url: url } }
+    end
+  end
 end

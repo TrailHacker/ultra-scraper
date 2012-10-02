6929822c172db82bdd676560c7edaf795147c9b4
TrailHacker <lucas@hardbarger.com>
2012-10-01 21:36:58 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.11.0'
+end
+
+group :test do
+  #gem 'capybara', '1.1.2' #TODO: FIX THIS!
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UltraScraper::Application.config.secret_token = '2ea000f43e2862596f87e1a7e410979722a8d03070c172ab6f53fb12963dcaa3f007abe7688b2cad4d8a2de90893955ccbe9543ad1029b81e12a07b46424a119'
